34832421aea530907ce468e195e4340fc43cde38
HHH <2982482549@qq.com>
2021-04-24 12:12:38 +0800

    New json core

---
+++ core/core.py
@@ -0,0 +1,16 @@
+#! -*- coding: utf-8 -*-
+import os, sys, shutil
+import zlib, json
+
+def dumpdict(filedict: dict, filename = None):
+    """Dict into json and write to the file
+       -
+       ({file='text1', file2='text2',
+        dir={'file1': 'text1', 'file2': 'text2'}})
+    """
+    
+    
+    
+    
+if __name__ == '__main__':
+
