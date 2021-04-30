c6afb8bc25196f1083c66ba368005f97954889b9
HHH <2982482549@qq.com>
2021-04-30 22:47:07 +0800

    Compress problem

--- core/core.py
+++ core/core.py
@@ -1,16 +1,49 @@
 #! -*- coding: utf-8 -*-
 import os, sys, shutil
-import zlib, json
+import zlib, json, io
+import logging
 
-def dumpdict(filedict: dict, filename = None):
-    """Dict into json and write to the file
-       -
-       ({file='text1', file2='text2',
-        dir={'file1': 'text1', 'file2': 'text2'}})
-    """
+class Notebook:
+    """A notebook by a dict
+    - {'file1': 'value1', 'file2': 'value2', 'list1': ['file1.1', 'file2.2']}"""
     
-    
-    
-    
+    def __init__(self, note_dict: dict):
+        self.note_dict = note_dict
+        
+    @staticmethod
+    def compress(to_comp: dict, chunk_size = 100000):
+        """Compresses: dict -> str -> encodedstr -> compedbytes"""
+        compressor = zlib.compressobj()
+        str_comp = str(to_comp)
+        encoded_comp = str_comp.encode()
+        
+        # Debug
+        logging.info(type(encoded_comp))
+        
+        # Compress through iteration
+        comped = b''
+        if len(encoded_comp) <= chunk_size:
+            comped += compressor.compress(encoded_comp)
+            
+        else:
+            i = 0
+            for chunk in encoded_comp[i : i + chunk_size]:
+                comped += compressor.compress(chunk)
+                i += chunk_size
+                
+                if len(encoded_comp) < i + chunk_size:
+                    # No enough bytes to index
+                    # Always goes here in the end
+                    comped += compressor.compress(encoded_comp[i:])
+                    break
+                    
+        # Return
+        return comped
+        
+
 if __name__ == '__main__':
-
+    test_notebook1 = Notebook(
+        {'file1': 'value1', 'file2': 'value2', 'list1': ['file1.1', 'file2.2']}
+    )
+    print(Notebook.compress(test_notebook1.note_dict))
+    print(Notebook.compress(test_notebook1.note_dict, 2))
